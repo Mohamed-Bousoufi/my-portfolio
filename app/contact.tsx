390aabf91f5f69ae2adc21f44d132247aabccc81
Mohamed-Bousoufi <lahmami96@gmail.com>
2026-02-14 22:04:28 +0100

    [FINISH] V3

--- app/contact.tsx
+++ app/contact.tsx
@@ -8,6 +8,8 @@ import { sendEmail } from "./actions/MailSender";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Github, Linkedin } from "lucide-react";
 
 const user = z.object({
     name : z.string().min(3, "Name must be at least 3 characters").max(50, "Name must be less than 50 characters"),
@@ -47,8 +49,18 @@ function Contact(){
   }
 
     return (
-        <div className="flex flex-col md:flex-row-reverse items-center justify-around gap-4 sm:gap-8 md:gap-16 w-full p-4 sm:p-8 md:p-16 h-[80vh]">
+        <div className="flex flex-col md:flex-row-reverse items-center justify-around  gap-10 sm:gap-8 md:gap-16 w-full p-4 sm:p-8 md:p-16 h-[80vh]">
         <div className="w-full md:max-w-fit max-h-fit">
+            <div className=" relative -top-2 flex flex-row  items-center justify-center gap-4 p-2">
+            <Button
+                onClick={()=> window.open('https://www.linkedin.com/in/bousoufi-mohamed-ab3aa8252/', '_blank')}
+                size={"lg"} className="self-center flex flex-row items-center justify-center heartbeateffect bg-transparent text-foreground border border-2 border-secondary font-bold bg-gradient-to-r from-secondary to-primary bg-clip-text text-transparent px-2 sm:px-4 text-xs sm:text-sm">
+                Linkedin <Linkedin className="w-4 h-4 sm:w-5 sm:h-5 text-secondary" /></Button>
+            <Button
+                onClick={()=>window.open('https://github.com/Mohamed-Bousoufi','_blank')}
+                size={"lg"} className="self-center flex flex-row items-center justify-center heartbeateffect bg-transparent text-foreground border border-2 border-secondary font-bold bg-gradient-to-r from-secondary to-primary bg-clip-text text-transparent px-2 sm:px-4 text-xs sm:text-sm">
+                Github <Github className="w-4 h-4 sm:w-5 sm:h-5 text-secondary" /></Button>
+            </div>
             <form className="flex flex-col gap-3 sm:gap-5 rounded-md border-muted-foreground border-2 p-3 sm:p-6" onSubmit={handleSubmit(onSubmit)}>
                     {SuccessSubmit && <p className="text-green-500 text-sm">Message sent successfully!</p>}
                     {ErrorSubmit && <p className="text-destructive text-sm">Failed to send message. Please try again.</p>}
@@ -70,9 +82,9 @@ function Contact(){
                 <button type="submit" className="border-2 border-primary rounded-md p-2 text-sm sm:text-base">Send</button>
             </form>
         </div>
-            <div className="relative flex border-l-0 md:border-l-2 border-muted-foreground">
-                <h1 className="absolute top-2 sm:top-4 md:top-10 left-3 md:-right-40 text-3xl sm:text-2xl md:text-7xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-destructive via-primary to-secondary -z-20">Connect With Me 👋</h1>
-                <Lottie animationData={animationData} loop={true} className="self-start w-60 h-60 sm:w-60 sm:h-60 md:w-1xl md:h-1xl lg:w-1xl lg:h-1xl"/>
+            <div className="relative flex border-l-0 md:border-l-2 border-muted-foreground h-fit items-center justify-between">
+                <h1 className="absolute -top-10 sm:top-4 md:-top-28 -left-10 md:-right-30 text-7xl sm:text-7xl md:text-7xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-destructive via-primary to-secondary -z-20">Connect With Me</h1>
+                <Lottie animationData={animationData} loop={true} className="self-start w-60 h-60 sm:w-60 sm:h-60 md:w-1xl md:h-1xl lg:w-1xl lg:h-1xl left-5"/>
             </div>
         </div>
     );
